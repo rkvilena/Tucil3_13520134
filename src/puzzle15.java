5492d7ce0b7453cac0135c3aba17964014c1ce86
rkvilena <rwad0711@gmail.com>
2022-03-28 23:01:42 +0700

    Add movement algorithm & comment

--- src/puzzle15.java
+++ src/puzzle15.java
@@ -5,15 +5,16 @@ class puzzle15{
     private int[][] puzzle;
     private char[] moveDirection;
     private boolean usingFile;
-    private ArrayList<int[][]> possiblePath;
+    private ArrayList<PuzzleState> possiblePath;
     private int Xabsis;
     private int Xordinat;
     private int[][] gridcolor;
 
+    // Konstruktor kelas puzzle15
     public puzzle15(boolean uF){
         this.puzzle = new int[4][4];
         this.usingFile = uF;
-        this.possiblePath = new ArrayList<int[][]>();
+        this.possiblePath = new ArrayList<PuzzleState>();
         this.Xabsis = -1;
         this.Xordinat = -1;
         this.moveDirection = new char[]{'U','R','D','L'};
@@ -23,7 +24,7 @@ class puzzle15{
         {0,1,0,1},
         {1,0,1,0}};
 
-        if (uF){ // Input persoalan melalui file
+        if (this.usingFile){ // Input persoalan melalui file
             Scanner input = new Scanner(System.in);
             String file = input.nextLine();
             this.puzzle = FileProcess.matrixProcessing(file);
@@ -44,6 +45,7 @@ class puzzle15{
         }
     }
 
+    // Method untuk tampilkan matriks puzzle
     private void displayPuzzle(int[][] puzzle){
         for (int i =  0; i < puzzle.length; i++){
             for (int j =  0; j < puzzle[0].length; j++){
@@ -56,15 +58,13 @@ class puzzle15{
         }
     }
 
+    // Method implementasi KURANG(i)
     private int kurang(int[][] onepuzzle){
         int kurangI = 0;
         int[] puzzleseq = matToArr(onepuzzle);
         for (int i = 0; i < puzzleseq.length; i++){
             for (int j = i + 1; j < puzzleseq.length; j++){
                 if (puzzleseq[i] > puzzleseq[j] && puzzleseq[j] != 0 && puzzleseq[i] != 0){
-                    System.out.println(puzzleseq[i]);
-                    System.out.println(puzzleseq[j]);
-                    System.out.println();
                     kurangI++;
                 }
                 if (puzzleseq[i] == 0){
@@ -75,6 +75,8 @@ class puzzle15{
         return kurangI;
     }
 
+    // Method ubah matriks menjadi array
+    // Khusus digunakan untuk perhitungan KURANG(i)
     private int[] matToArr(int[][] onepuzzle){
         int size = onepuzzle.length * onepuzzle.length;
         int k = 0;
@@ -92,10 +94,14 @@ class puzzle15{
         return sequence;
     }
     
+    // Method untuk menentukan nilai X
     private int valueX(int i, int j){
         return (this.gridcolor[i][j]);
     }
 
+    // Method untuk menentukan bisa tidaknya puzzle diselesaikan
+    // Mengembalikan true apabila bisa
+    // Mengembalikan false apabila tidak bisa
     private boolean isReachable(int[][] onepuzzle){
         int theoremresult = this.kurang(onepuzzle);
         theoremresult += this.valueX(this.Xabsis, this.Xordinat);
@@ -106,12 +112,64 @@ class puzzle15{
         return false;
         
     }
-    /*
+
+    // Method utama untuk penyelesaian puzzle
     public void solve(){
+        /*
         for (char direction : this.moveDirection){ 
-            statements using var;
+            if ()
+            this.possiblePath.add(this.puzzle);
         }
-    }*/
+        */
+    }
+
+    // Method untuk membangkitkan semua kemungkinan langkah
+    private int[][] move(int[][] puzzle, int zeroX, int zeroY){
+        int[][] arisedstate = puzzle;
+        int x = zeroX, y = zeroY;
+        int temp = arisedstate[x][y];
+        for (int i = 0; i < this.moveDirection.length; i++){
+            switch(this.moveDirection[i]) {
+                case 'U':
+                    if (x != 0){
+                        arisedstate[x][y] = arisedstate[x-1][y];
+                        arisedstate[x-1][y] = temp;
+                        PuzzleState possiblestate = new PuzzleState(arisedstate, 0, x-1, y);
+                        this.possiblePath.add(possiblestate);
+                        arisedstate = puzzle;
+                    }
+                    break;
+                case 'R':
+                    if (y != arisedstate[0].length - 1){
+                        arisedstate[x][y] = arisedstate[x][y+1];
+                        arisedstate[x][y+1] = temp;
+                        PuzzleState possiblestate = new PuzzleState(arisedstate, 0, x, y+1);
+                        this.possiblePath.add(possiblestate);
+                        arisedstate = puzzle;
+                    }
+                    break;
+                case 'D':
+                    if (x != arisedstate.length - 1){
+                        arisedstate[x][y] = arisedstate[x+1][y];
+                        arisedstate[x+1][y] = temp;
+                        PuzzleState possiblestate = new PuzzleState(arisedstate, 0, x+1, y);
+                        this.possiblePath.add(possiblestate);
+                        arisedstate = puzzle;
+                    }
+                    break;
+                case 'L':
+                    if (y != 0){
+                        arisedstate[x][y] = arisedstate[x][y-1];
+                        arisedstate[x][y-1] = temp;
+                        PuzzleState possiblestate = new PuzzleState(arisedstate, 0, x, y-1);
+                        this.possiblePath.add(possiblestate);
+                        arisedstate = puzzle;
+                    }
+                    break;
+            }
+        }
+        return arisedstate;
+    }
 
     private int f(int[][] puzzle){
         return 0;
@@ -122,9 +180,33 @@ class puzzle15{
 
     public static void main(String[] args) {
         puzzle15 test = new puzzle15(true);
+        /*
         test.possiblePath.add(test.puzzle);
         test.displayPuzzle(test.possiblePath.get(0));
         System.out.println(test.isReachable(test.possiblePath.get(0)));
+        */
 
+    }
+}
+
+// Kelas untuk menampung data hasil pembangkitan
+// Bertujuan untuk efisiensi waktu algoritma
+class PuzzleState{
+    public int[][] instancepuzzle;
+    public int cost;
+    public int x0;
+    public int y0;
+
+    public PuzzleState(){
+        this.instancepuzzle = new int[4][4];
+        this.cost = 0;
+        this.x0 = -1;
+        this.y0 = -1;
+    }
+    public PuzzleState(int[][] p, int c, int x, int y){
+        this.instancepuzzle = p;
+        this.cost = c;
+        this.x0 = x;
+        this.y0 = y;
     }
 }
